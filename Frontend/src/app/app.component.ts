5a55e44ae9cce04989a6235ceb3f01452e1c184d
Sebastian Born <vezixig@gmail.com>
2025-08-16 08:13:32 +0200

    Created main layout and added navigation bar

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
-
+import { MenubarModule } from 'primeng/menubar';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule, MenubarModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
